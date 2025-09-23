d7380f06951291ec3a1d9d6d3f8e402a1c1ca991
pulce2011 <filippofifafut@gmail.com>
2025-09-23 19:13:57 +0200

    Bug fix

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,7 +8,6 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', RegisterView.as_view(), name='register'),
     path('api/', include('core.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ExerciseListView, RunExerciseView, CourseListView
+from .views import ExerciseListView, RunExerciseView, CourseListView, RegisterView
 
 urlpatterns = [
     path('exercises/', ExerciseListView.as_view(), name='exercise-list'),
     path('run/', RunExerciseView.as_view(), name='run-exercise'),
     path('courses/', CourseListView.as_view(), name='course-list'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
 
