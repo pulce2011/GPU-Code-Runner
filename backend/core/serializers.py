71baa9840ae7bc9a292414168f1aac226b4b74c8
pulce2011 <filippofifafut@gmail.com>
2025-09-26 16:50:53 +0200

    Created Task serializer

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Exercise, User, Course
+from .models import Exercise, User, Course, Task
 
 # Serializzatore per gli esercizi
 class ExerciseSerializer(serializers.ModelSerializer):
@@ -26,7 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ['id', 'email', 'matr', 'first_name', 'last_name', 'course', 'course_name', 'is_superuser', 'password']
+        fields = ['id', 'email', 'matr', 'first_name', 'last_name', 'course', 'course_name', 'is_superuser', 'credits', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }
@@ -37,3 +37,18 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)  # hash password
         user.save()
         return user
+
+
+# Serializzatore per i task
+class TaskSerializer(serializers.ModelSerializer):
+    user_matr = serializers.CharField(source='user.matr', read_only=True)
+    user_name = serializers.CharField(source='user.get_full_name', read_only=True)
+    exercise_name = serializers.CharField(source='exercise.name', read_only=True)
+    
+    class Meta:
+        model = Task
+        fields = ['id', 'user', 'user_matr', 'user_name', 'exercise', 'exercise_name', 'status', 
+                'created_at', 'started_at', 'finished_at', 'total_execution_time', 
+                'stdout', 'stderr', 'credits_cost', 'process_id']
+        read_only_fields = ['id', 'user', 'created_at', 'started_at', 'finished_at', 
+                            'total_execution_time', 'stdout', 'stderr', 'process_id']
