8d9e8f02a41eb548543f162b7ca8dd38786f172c
pulce2011 <filippofifafut@gmail.com>
2025-09-26 17:18:41 +0200

    Updated TaskAdmin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course, User, Exercise
+from .models import Course, User, Exercise, Task
 
 
 # Configurazione admin per gestire i corsi universitari
@@ -35,9 +35,9 @@ class ExerciseAdmin(admin.ModelAdmin):
     def get_courses(self, obj):
         return ", ".join([course.name for course in obj.courses.all()])
     get_courses.short_description = 'Courses'
-    
 
-# Configurazione admin per gestire task
+
+# Configurazione admin per gestire i task
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ['user__matr', 'exercise__name', 'status', 'created_at', 'started_at', 'finished_at']
@@ -48,5 +48,5 @@ class TaskAdmin(admin.ModelAdmin):
     fieldsets = (
         ('Info', {'fields': ('id', 'user', 'exercise', 'status')}),
         ('Output', {'fields': ('stdout', 'stderr')}),
-        ('Timestamps', {'fields': ('created_at', 'started_at', 'finished_at')}),
+        ('Timestamps', {'fields': ('created_at', 'started_at', 'finished_at', 'total_execution_time')}),
     )
