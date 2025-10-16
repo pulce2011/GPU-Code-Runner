c01bca52ffa55b832f244283594ac3631b2a2725
pulce2011 <filippofifafut@gmail.com>
2025-10-16 16:14:49 +0200

    Bash script debug info

--- backend/simulate_gpu.sh
+++ backend/simulate_gpu.sh
@@ -2,13 +2,20 @@
 # simulate_gpu.sh
 # $1 = percorso file codice da eseguire (temporaneo)
 
-#sleep_duration=$((RANDOM % 7))
-sleep_duration=20
+echo "> [INFO]Script args:"
+for arg in "$@"; do
+echo "> [INFO] $arg"
+done
+
+sleep_duration=$((RANDOM % 7))
+#sleep_duration=20
 echo "> Durata sleep: $sleep_duration secondi"
 
 sleep $sleep_duration
 echo "> [DEBUG] Standard output"
 echo "> [DEBUG] Standard error" >&2
+
+
 
 # Genera un numero casuale per determinare se il task è riuscito o fallito
 exit $((RANDOM % 2))
