f8b88405840f726a37dd00736f2cd3b60815870d
pulce2011 <filippofifafut@gmail.com>
2025-10-17 16:32:34 +0200

    Bug fix

--- backend/core/routing.py
+++ backend/core/routing.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 from .websocket import TaskConsumer
 
 websocket_urlpatterns = [
-    re_path(r'^ws/tasks/(?P<task_id>\\d+)/$', TaskConsumer.as_asgi()),
+    re_path(r'^ws/tasks/(?P<task_id>\d+)/$', TaskConsumer.as_asgi()),
 ]
 
 
